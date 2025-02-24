6ea3f93e7c7e002a9005636de40a353de942bc42
PhamQuocTai <phamtien123bt@gmail.com>
2025-02-24 10:32:06 +0700

    init project

---
+++ src/main/java/com/example/PhamQuocTai/PhamQuocTaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.PhamQuocTai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamQuocTaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamQuocTaiApplication.class, args);
+	}
+
+}
